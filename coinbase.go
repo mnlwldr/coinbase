be9d523868069b6f955a76ff76658b7beb4c4de7
mnlwldr <mnlwldr@gmail.com>
2022-02-16 09:35:31 +0100

    Add func GetWithDate(coin, date string)

--- coinbase.go
+++ coinbase.go
@@ -23,8 +23,12 @@ func buildUrl(coin string) string {
 	return fmt.Sprintf("%s/%s/%s", COINBASE_API_URL, coin, TYPE)
 }
 
-func get(coin string) ([]byte, error) {
-	response, err := http.Get(buildUrl(coin))
+func buildUrlWithDate(coin, date string) string {
+	return fmt.Sprintf("%s/%s/%s?date=%s", COINBASE_API_URL, coin, TYPE, date)
+}
+
+func get(url string) ([]byte, error) {
+	response, err := http.Get(url)
 	if err != nil {
 		return []byte{}, err
 	}
@@ -36,9 +40,21 @@ func get(coin string) ([]byte, error) {
 }
 
 // The coin abbreviation is passed as parameter "coin".
+// The date is in the format YYYY-MM-DD
+func GetWithDate(coin, date string) (Response, error) {
+	url := buildUrlWithDate(coin, date)
+	return buildResponse(url)
+}
+
+// The coin abbreviation is passed as parameter "coin".
 // The coin abbreviation is a currency pair. For example: "SHIB-EUR" or "BTC-USD"
 func Get(coin string) (Response, error) {
-	response, err := get(coin)
+	url := buildUrl(coin)
+	return buildResponse(url)
+}
+
+func buildResponse(url string) (Response, error) {
+	response, err := get(url)
 	if err != nil {
 		return Response{}, err
 	}
--- coinbase_test.go
+++ coinbase_test.go
@@ -5,21 +5,39 @@ import (
 	"testing"
 )
 
-func TestTranslate(t *testing.T) {
+func TestGet(t *testing.T) {
 
 	wantCoin := "SHIB"
 	wantCurrency := "EUR"
 
 	response, _ := Get(fmt.Sprintf("%s-%s", wantCoin, wantCurrency))
-
-	fmt.Printf("%.7f\n", response.Amount)
-
 	if response.Base != wantCoin {
 		t.Errorf("Translate() = %q, want %q", response.Base, wantCoin)
 	}
 
 	if response.Currency != wantCurrency {
-		t.Errorf("Translate() = %q, want %q", response.Currency, wantCoin)
+		t.Errorf("TestGet() = %q, want %q", response.Currency, wantCoin)
+	}
+
+}
+
+func TestGetWithDate(t *testing.T) {
+
+	wantCoin := "SHIB"
+	wantCurrency := "EUR"
+	wantAmount := 0.0000190
+
+	response, _ := GetWithDate(fmt.Sprintf("%s-%s", wantCoin, wantCurrency), "2022-02-01")
+	if response.Base != wantCoin {
+		t.Errorf("Translate() = %q, want %q", response.Base, wantCoin)
+	}
+
+	if response.Currency != wantCurrency {
+		t.Errorf("TestGet() = %q, want %q", response.Currency, wantCoin)
+	}
+
+	if response.Amount != wantAmount {
+		t.Errorf("TestGet() = %.7f, want %.7f", response.Amount, wantAmount)
 	}
 
 }
